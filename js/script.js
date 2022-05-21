a2b44dedd10d2fe364d4b36d48e1c773585474c7
Пользователь <dinarastamkulova4@gmail.com>
2022-05-22 01:08:50 +0600

    finished dark-light mode

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ function lightMode() {
     let element = document.getElementById("sun")
     element.style.backgroundColor = "#FFFFFF"
     element.style.color = "#222222"
+    document.getElementById('light-mode').style.color = "#222222"
 }
 
 function NightMode() {
     let element = document.getElementById("sun")
     element.style.backgroundColor = "#222222"
     element.style.color = "#FFFFFF"
+    document.getElementById('light-mode').style.color = "#FFFFFF"
 }
