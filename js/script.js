bd1f2634d52c17ec8a96a3da304d99efb92bbcfc
Пользователь <dinarastamkulova4@gmail.com>
2022-05-22 00:51:04 +0600

    add modal-window and dark-night mode

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.slide = new SlideNav();
+
+function lightMode() {
+    let element = document.getElementById("sun")
+    element.style.backgroundColor = "#FFFFFF"
+    element.style.color = "#222222"
+}
+
+function NightMode() {
+    let element = document.getElementById("sun")
+    element.style.backgroundColor = "#222222"
+    element.style.color = "#FFFFFF"
+}
