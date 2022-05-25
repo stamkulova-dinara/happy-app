6dcacc1f61a3af6b16d9bb0fabd7441b8d343d5d
Пользователь <dinarastamkulova4@gmail.com>
2022-05-25 14:22:13 +0600

    get data for block-pricing

--- src/js/plans.js
+++ src/js/plans.js
@@ -1,7 +1,17 @@
 import config from '../../data/config.json' assert {type: "json"}
  
 function getPlans() {
-    const tarif = config.plans
+    const [ standart, premium, lifetime ] = config.plans;
+
+    function tarifPlans({ name, price }) {
+       document.querySelector(`.${name}`).innerText = name;
+       document.querySelector(`.${name}-price`).innerText = `$${price}`;
+    }
+
+    tarifPlans(standart);
+    tarifPlans(premium);
+    tarifPlans(lifetime);
+
 }
 
 getPlans()
